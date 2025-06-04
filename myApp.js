53ce231d5108e651aa4aaefae8c93127fa0cc34b
Chanon Upatham <chanon.upa@gmail.com>
2025-06-04 04:23:13 +0000

    use .env

--- myApp.js
+++ myApp.js
@@ -1,5 +1,27 @@
+require('dotenv').config()
+const { response } = require('express');
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
+console.log('Hello World');
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get('/json', function (req, res) {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({
+            "message": "HELLO JSON"
+        })
+    } else {
+        res.json({
+            "message": "Hello json"
+        })
+    };
+});
 
 
 
@@ -30,10 +52,4 @@ let app = express();
 
 
 
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
