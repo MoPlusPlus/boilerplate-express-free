858f94b0342298fb36f63d54b825d6a087116ff3
Chanon Upatham <chanon.upa@gmail.com>
2025-06-07 03:23:49 +0000

    pass 9/12

--- myApp.js
+++ myApp.js
@@ -11,6 +11,43 @@ app.get("/", (req, res) => {
 
 app.use("/public", express.static(__dirname + '/public'));
 
+app.use(function middleware(req, res, next) {
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    next();
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+
 app.get('/json', function (req, res) {
     if (process.env.MESSAGE_STYLE === 'uppercase') {
         res.json({
@@ -19,7 +56,7 @@ app.get('/json', function (req, res) {
     } else {
         res.json({
             "message": "Hello json"
-            
+
         })
     };
 });
